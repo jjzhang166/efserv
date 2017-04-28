db355578c4bf97d722467eb5f33f79fd830af835
xiaozhuai <798047000@qq.com>
2017-04-28 18:21:00 +0800

    optimize project struct

---
+++ cmake_find_libeio.cmake
@@ -0,0 +1,30 @@
+# Try find libeio
+# LIBEIO_FOUND        - system has libeio
+# LIBEIO_INCLUDE_DIRS - libeio include directories
+# LIBEIO_LIBRARIES    - libraries needed to use libeio
+if(LIBEIO_INCLUDE_DIRS AND LIBEIO_LIBRARIES)
+    set(LIBEIO_FIND_QUIETLY TRUE)
+else()
+    find_path(
+            LIBEIO_INCLUDE_DIR
+            NAMES eio.h
+            HINTS ${LIBEIO_ROOT_DIR}
+            PATH_SUFFIXES include)
+    find_library(
+            LIBEIO_LIBRARY
+            NAME eio
+            HINTS ${LIBEIO_ROOT_DIR}
+            PATH_SUFFIXES ${CMAKE_INSTALL_LIBDIR})
+    set(LIBEIO_INCLUDE_DIRS ${LIBEIO_INCLUDE_DIR})
+    set(LIBEIO_LIBRARIES ${LIBEIO_LIBRARY})
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(
+            libeio DEFAULT_MSG LIBEIO_LIBRARY LIBEIO_INCLUDE_DIR)
+    mark_as_advanced(LIBEIO_LIBRARY LIBEIO_INCLUDE_DIR)
+endif()
+if(LIBEIO_FOUND)
+    message(STATUS "libeio found")
+else()
+    message(FATAL_ERROR "libeio not found")
+endif()
+# End of find libeio
